ebd090e3650cfee2ea0edfb661bfe29b229cb3a6
reclusivestar <tanmaysk1@gmail.com>
2020-12-31 01:58:20 -0800

    cors fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/*', (req, res) => {
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://portfolio-tanmay.herokuapp.com");
   res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET");
   next();
