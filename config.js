3153db9dd7bbf84cbf482e50e27531c39e33b5d8
reclusivestar <tanmaysk1@gmail.com>
2021-01-09 20:03:48 -0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clientId: '490770668294-vp0vahcsdq6mbe9qt7u2l640p0vket9d.apps.googleusercontent.com',
-    clientSecret: 'XLpWd37rkx3sXlLkhgzdGG9k',
-    refreshToken: '1//04ZNhtmgeGHZRCgYIARAAGAQSNwF-L9IrxiQK94atlNiuL8KtJIUgpX1uUmREUy3pxToIHJbPixXYTQ2_P1mBoGGWao0fSoPTd1Y',
-    user: 'tanmaysk1@gmail.com'
-}
